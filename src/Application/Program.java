6521127075c4a580d6883ef54c2ed771637d151a
Thieny Moda <thienyam@hotmail.com>
2026-07-04 23:31:59 -0300

    Seller entities class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,13 +1,20 @@
 package Application;
 
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Departament obj = new Departament(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Thieny", "thienyam@hotmail.com", new Date(), 3000.00, obj);
+		
+		
+		System.out.println(seller);
 	}
 
 }
