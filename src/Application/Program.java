2194e3913ad524fbefcf567c014ee66fd1482dd6
Thieny Moda <thienyam@hotmail.com>
2026-07-08 21:27:41 -0300

    Insert Implementation

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,5 +1,6 @@
 package Application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.Dao.DaoFactory;
@@ -31,7 +32,10 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		
+		System.out.println("\n=== TEST 4: Seller Insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0,departament);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New Id = " + newSeller.getId());
 	}
 
 }
